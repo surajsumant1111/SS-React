d35bb1055de809e915718dcdeeff8422443a8d6a
Suraj Kumar <surajsumant1111@gmail.com>
2025-07-18 23:32:08 +0530

    tailwind data

--- 03TailVindProp/src/App.jsx
+++ 03TailVindProp/src/App.jsx
@@ -6,11 +6,16 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let suraj={
+    user:"sumant",
+    age:28
+  }
 
   return (
     <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl'>TailWind CSS</h1>
-    <Card/>
+     <h1 className='bg-green-400 text-black p-2 rounded-xl'>TailWind CSS</h1><br/>
+    {/* <Card user="suraj" users={suraj}/><br/> */}
+    <Card btnText1="Click Me" btnText2="discard"/>
     <Card/>
     </>
   )
--- 03TailVindProp/src/components/Card.jsx
+++ 03TailVindProp/src/components/Card.jsx
@@ -22,84 +22,203 @@ import React from 'react'
 
 
 
-import { useState } from 'react'
+// import { useState } from 'react'
 
-        function Card() {
-          const [play, setPlay] = useState(false)
-          return (
-            <div className="border  w-[350px] glass rounded-xl flex">
-              <img
-                src="https://media4.giphy.com/media/BKv6EPd7ZCgmMg0iQK/giphy.gif?cid=ecf05e47dwh9vdr5bezeb3lbg1bqy8y3kfe61f2oopjxa2jw&rid=giphy.gif&ct=g"
-                alt="music"
-                width="150"
-                height="150"
-                className="rounded-2xl p-2"
-              />
+//         function Card() {
+//           const [play, setPlay] = useState(false)
+//           return (
+//             <div className="border  w-[350px] glass rounded-xl flex">
+//               <img
+//                 src="https://media4.giphy.com/media/BKv6EPd7ZCgmMg0iQK/giphy.gif?cid=ecf05e47dwh9vdr5bezeb3lbg1bqy8y3kfe61f2oopjxa2jw&rid=giphy.gif&ct=g"
+//                 alt="music"
+//                 width="150"
+//                 height="150"
+//                 className="rounded-2xl p-2"
+//               />
         
-              <div className="px-4 py-4 ">
-                <div className="font-outerSans py-3">
-                  <h1>Toxic</h1>
-                  <p className="text-xs">BoyWithUke</p>
-                </div>
-                <div className="flex w-full justify-evenly  ">
-                  <button className="fill-[#ffffff4d] mx-2 border rounded-full border-gray-500 p-1">
-                    <svg
-                      width="30"
-                      height="30"
-                      focusable="false"
-                      aria-hidden="true"
-                      viewBox="0 0 24 24"
-                      data-testid="FastRewindRoundedIcon"
-                    >
-                      <path d="M11 16.07V7.93c0-.81-.91-1.28-1.58-.82l-5.77 4.07c-.56.4-.56 1.24 0 1.63l5.77 4.07c.67.47 1.58 0 1.58-.81zm1.66-3.25 5.77 4.07c.66.47 1.58-.01 1.58-.82V7.93c0-.81-.91-1.28-1.58-.82l-5.77 4.07c-.57.4-.57 1.24 0 1.64z"></path>
-                    </svg>
-                  </button>
+//               <div className="px-4 py-4 ">
+//                 <div className="font-outerSans py-3">
+//                   <h1>Toxic</h1>
+//                   <p className="text-xs">BoyWithUke</p>
+//                 </div>
+//                 <div className="flex w-full justify-evenly  ">
+//                   <button className="fill-[#ffffff4d] mx-2 border rounded-full border-gray-500 p-1">
+//                     <svg
+//                       width="30"
+//                       height="30"
+//                       focusable="false"
+//                       aria-hidden="true"
+//                       viewBox="0 0 24 24"
+//                       data-testid="FastRewindRoundedIcon"
+//                     >
+//                       <path d="M11 16.07V7.93c0-.81-.91-1.28-1.58-.82l-5.77 4.07c-.56.4-.56 1.24 0 1.63l5.77 4.07c.67.47 1.58 0 1.58-.81zm1.66-3.25 5.77 4.07c.66.47 1.58-.01 1.58-.82V7.93c0-.81-.91-1.28-1.58-.82l-5.77 4.07c-.57.4-.57 1.24 0 1.64z"></path>
+//                     </svg>
+//                   </button>
         
-                  <button
-                    className="fill-white border rounded-full border-gray-500 p-1"
-                    onClick={() => setPlay(!play)}
-                  >
-                    {play === true ? (
-                      <svg
-                        width="30"
-                        height="30"
-                        focusable="false"
-                        aria-hidden="true"
-                        viewBox="0 0 24 24"
-                        data-testid="PlayArrowRoundedIcon"
-                      >
-                        <path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path>
-                      </svg>
-                    ) : (
-                      <svg
-                        width="30"
-                        height="30"
-                        focusable="false"
-                        aria-hidden="true"
-                        viewBox="0 0 24 24"
-                        data-testid="PauseRoundedIcon"
-                      >
-                        <path d="M8 19c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2v10c0 1.1.9 2 2 2zm6-12v10c0 1.1.9 2 2 2s2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2z"></path>
-                      </svg>
-                    )}
-                  </button>
+//                   <button
+//                     className="fill-white border rounded-full border-gray-500 p-1"
+//                     onClick={() => setPlay(!play)}
+//                   >
+//                     {play === true ? (
+//                       <svg
+//                         width="30"
+//                         height="30"
+//                         focusable="false"
+//                         aria-hidden="true"
+//                         viewBox="0 0 24 24"
+//                         data-testid="PlayArrowRoundedIcon"
+//                       >
+//                         <path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path>
+//                       </svg>
+//                     ) : (
+//                       <svg
+//                         width="30"
+//                         height="30"
+//                         focusable="false"
+//                         aria-hidden="true"
+//                         viewBox="0 0 24 24"
+//                         data-testid="PauseRoundedIcon"
+//                       >
+//                         <path d="M8 19c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2v10c0 1.1.9 2 2 2zm6-12v10c0 1.1.9 2 2 2s2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2z"></path>
+//                       </svg>
+//                     )}
+//                   </button>
         
-                  <button className="fill-[#ffffff4d] mx-2  border rounded-full border-gray-500 p-1">
-                    <svg
-                      width="30"
-                      height="30"
-                      focusable="false"
-                      aria-hidden="true"
-                      viewBox="0 0 24 24"
-                      data-testid="FastForwardRoundedIcon"
-                    >
-                      <path d="m5.58 16.89 5.77-4.07c.56-.4.56-1.24 0-1.63L5.58 7.11C4.91 6.65 4 7.12 4 7.93v8.14c0 .81.91 1.28 1.58.82zM13 7.93v8.14c0 .81.91 1.28 1.58.82l5.77-4.07c.56-.4.56-1.24 0-1.63l-5.77-4.07c-.67-.47-1.58 0-1.58.81z"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-          )
-        }
+//                   <button className="fill-[#ffffff4d] mx-2  border rounded-full border-gray-500 p-1">
+//                     <svg
+//                       width="30"
+//                       height="30"
+//                       focusable="false"
+//                       aria-hidden="true"
+//                       viewBox="0 0 24 24"
+//                       data-testid="FastForwardRoundedIcon"
+//                     >
+//                       <path d="m5.58 16.89 5.77-4.07c.56-.4.56-1.24 0-1.63L5.58 7.11C4.91 6.65 4 7.12 4 7.93v8.14c0 .81.91 1.28 1.58.82zM13 7.93v8.14c0 .81.91 1.28 1.58.82l5.77-4.07c.56-.4.56-1.24 0-1.63l-5.77-4.07c-.67-.47-1.58 0-1.58.81z"></path>
+//                     </svg>
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
+//           )
+//         }
 
-        export default Card
+//         export default Card
+
+
+
+
+
+import styled from 'styled-components';
+
+let Card = ({btnText1="Visit", btnText2="Diverse"}) => {
+  // console.log("props=",props)
+  return (    
+    <StyledWrapper>
+      <div className="card">
+        <svg xmlSpace="preserve" viewBox="0 0 122.88 122.25" y="0px" x="0px" id="cookieSvg" version="1.1"><g><path d="M101.77,49.38c2.09,3.1,4.37,5.11,6.86,5.78c2.45,0.66,5.32,0.06,8.7-2.01c1.36-0.84,3.14-0.41,3.97,0.95 c0.28,0.46,0.42,0.96,0.43,1.47c0.13,1.4,0.21,2.82,0.24,4.26c0.03,1.46,0.02,2.91-0.05,4.35h0v0c0,0.13-0.01,0.26-0.03,0.38 c-0.91,16.72-8.47,31.51-20,41.93c-11.55,10.44-27.06,16.49-43.82,15.69v0.01h0c-0.13,0-0.26-0.01-0.38-0.03 c-16.72-0.91-31.51-8.47-41.93-20C5.31,90.61-0.73,75.1,0.07,58.34H0.07v0c0-0.13,0.01-0.26,0.03-0.38 C1,41.22,8.81,26.35,20.57,15.87C32.34,5.37,48.09-0.73,64.85,0.07V0.07h0c1.6,0,2.89,1.29,2.89,2.89c0,0.4-0.08,0.78-0.23,1.12 c-1.17,3.81-1.25,7.34-0.27,10.14c0.89,2.54,2.7,4.51,5.41,5.52c1.44,0.54,2.2,2.1,1.74,3.55l0.01,0 c-1.83,5.89-1.87,11.08-0.52,15.26c0.82,2.53,2.14,4.69,3.88,6.4c1.74,1.72,3.9,3,6.39,3.78c4.04,1.26,8.94,1.18,14.31-0.55 C99.73,47.78,101.08,48.3,101.77,49.38L101.77,49.38z M59.28,57.86c2.77,0,5.01,2.24,5.01,5.01c0,2.77-2.24,5.01-5.01,5.01 c-2.77,0-5.01-2.24-5.01-5.01C54.27,60.1,56.52,57.86,59.28,57.86L59.28,57.86z M37.56,78.49c3.37,0,6.11,2.73,6.11,6.11 s-2.73,6.11-6.11,6.11s-6.11-2.73-6.11-6.11S34.18,78.49,37.56,78.49L37.56,78.49z M50.72,31.75c2.65,0,4.79,2.14,4.79,4.79 c0,2.65-2.14,4.79-4.79,4.79c-2.65,0-4.79-2.14-4.79-4.79C45.93,33.89,48.08,31.75,50.72,31.75L50.72,31.75z M119.3,32.4 c1.98,0,3.58,1.6,3.58,3.58c0,1.98-1.6,3.58-3.58,3.58s-3.58-1.6-3.58-3.58C115.71,34.01,117.32,32.4,119.3,32.4L119.3,32.4z M93.62,22.91c2.98,0,5.39,2.41,5.39,5.39c0,2.98-2.41,5.39-5.39,5.39c-2.98,0-5.39-2.41-5.39-5.39 C88.23,25.33,90.64,22.91,93.62,22.91L93.62,22.91z M97.79,0.59c3.19,0,5.78,2.59,5.78,5.78c0,3.19-2.59,5.78-5.78,5.78 c-3.19,0-5.78-2.59-5.78-5.78C92.02,3.17,94.6,0.59,97.79,0.59L97.79,0.59z M76.73,80.63c4.43,0,8.03,3.59,8.03,8.03 c0,4.43-3.59,8.03-8.03,8.03s-8.03-3.59-8.03-8.03C68.7,84.22,72.29,80.63,76.73,80.63L76.73,80.63z M31.91,46.78 c4.8,0,8.69,3.89,8.69,8.69c0,4.8-3.89,8.69-8.69,8.69s-8.69-3.89-8.69-8.69C23.22,50.68,27.11,46.78,31.91,46.78L31.91,46.78z M107.13,60.74c-3.39-0.91-6.35-3.14-8.95-6.48c-5.78,1.52-11.16,1.41-15.76-0.02c-3.37-1.05-6.32-2.81-8.71-5.18 c-2.39-2.37-4.21-5.32-5.32-8.75c-1.51-4.66-1.69-10.2-0.18-16.32c-3.1-1.8-5.25-4.53-6.42-7.88c-1.06-3.05-1.28-6.59-0.61-10.35 C47.27,5.95,34.3,11.36,24.41,20.18C13.74,29.69,6.66,43.15,5.84,58.29l0,0.05v0h0l-0.01,0.13v0C5.07,73.72,10.55,87.82,20.02,98.3 c9.44,10.44,22.84,17.29,38,18.1l0.05,0h0v0l0.13,0.01h0c15.24,0.77,29.35-4.71,39.83-14.19c10.44-9.44,17.29-22.84,18.1-38l0-0.05 v0h0l0.01-0.13v0c0.07-1.34,0.09-2.64,0.06-3.91C112.98,61.34,109.96,61.51,107.13,60.74L107.13,60.74z M116.15,64.04L116.15,64.04 L116.15,64.04L116.15,64.04z M58.21,116.42L58.21,116.42L58.21,116.42L58.21,116.42z" /></g></svg>
+        <p className="cookieHeading">We use cookies.</p>
+        <p className="cookieDescription">We use cookies to ensure that we give you the best experience on our website. <br /><a href="#">Read cookies policies</a>.</p>
+        <div className="buttonContainer">
+          <button className="acceptButton">{btnText1}</button>
+          <button className="declineButton">{btnText2}</button>
+        </div>
+      </div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  .card {
+    width: 300px;
+    height: 220px;
+    background-color: rgb(255, 255, 255);
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 30px;
+    gap: 13px;
+    position: relative;
+    overflow: hidden;
+    box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.062);
+  }
+
+  #cookieSvg {
+    width: 50px;
+  }
+
+  #cookieSvg g path {
+    fill: rgb(97, 81, 81);
+  }
+
+  .cookieHeading {
+    font-size: 1.2em;
+    font-weight: 800;
+    color: rgb(26, 26, 26);
+  }
+
+  .cookieDescription {
+    text-align: center;
+    font-size: 0.7em;
+    font-weight: 600;
+    color: rgb(99, 99, 99);
+  }
+
+  .cookieDescription a {
+    --tw-text-opacity: 1;
+    color: rgb(59 130 246);
+  }
+
+  .cookieDescription a:hover {
+    -webkit-text-decoration-line: underline;
+    text-decoration-line: underline;
+  }
+
+  .buttonContainer {
+    display: flex;
+    gap: 20px;
+    flex-direction: row;
+  }
+
+  .acceptButton {
+    width: 80px;
+    height: 30px;
+    background-color: #7b57ff;
+    transition-duration: .2s;
+    border: none;
+    color: rgb(241, 241, 241);
+    cursor: pointer;
+    font-weight: 600;
+    border-radius: 20px;
+    box-shadow: 0 4px 6px -1px #977ef3, 0 2px 4px -1px #977ef3;
+    transition: all .6s ease;
+  }
+
+  .declineButton {
+    width: 80px;
+    height: 30px;
+    background-color: #dadada;
+    transition-duration: .2s;
+    color: rgb(46, 46, 46);
+    border: none;
+    cursor: not-allowed;
+    font-weight: 600;
+    border-radius: 20px;
+    box-shadow: 0 4px 6px -1px #bebdbd, 0 2px 4px -1px #bebdbd;
+    transition: all .6s ease;
+  }
+
+  .declineButton:hover {
+    background-color: #ebebeb;
+    box-shadow: 0 10px 15px -3px #bebdbd, 0 4px 6px -2px #bebdbd;
+    transition-duration: .2s;
+  }
+
+  .acceptButton:hover {
+    background-color: #9173ff;
+    box-shadow: 0 10px 15px -3px #977ef3, 0 4px 6px -2px #977ef3;
+    transition-duration: .2s;
+  }`;
+
+export default Card;
